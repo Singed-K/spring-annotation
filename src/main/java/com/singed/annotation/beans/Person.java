492fe68d1afca690ff7c6753cb7d1358916d15f9
SuperKai <307140566@qq.com>
2021-09-07 20:03:34 +0800

    spring-annotation

---
+++ src/main/java/com/singed/annotation/beans/Person.java
@@ -0,0 +1,42 @@
+package com.singed.annotation.beans;
+
+/**
+ * @Author : Singed
+ * @Date : 2021/9/7 19:53
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
